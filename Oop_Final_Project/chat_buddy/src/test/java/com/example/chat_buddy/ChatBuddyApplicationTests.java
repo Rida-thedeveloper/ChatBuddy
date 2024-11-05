dc87bdd01972f367d556b4b3d61f384d9b10c2ea
Rida-thedeveloper <ridathedeveloper@gmail.com>
2024-11-05 10:32:57 +0500

    Add files via upload

---
+++ Oop_Final_Project/chat_buddy/src/test/java/com/example/chat_buddy/ChatBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
